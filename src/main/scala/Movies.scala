66f6a3e580c52c039475785f67e5991fa7f6ff19
Dorota00 <dorotazub22@gmail.com>
2022-05-24 09:42:31 +0200

    Editing project

--- src/main/scala/Movies.scala
+++ src/main/scala/Movies.scala
@@ -2,19 +2,20 @@ package agh
 
 import java.time.Year
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.DataFrame
 
 object Movies extends SparkSessionProvider {
 
-  def main(args: Array[String]): DataFrame = {
+  def main(args: Array[String]): Unit = {
     val path = args(0)
 
-    spark.read.format("csv")
+    val df = spark.read.format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load(path)
       .na.drop()
       .withColumn("years_passed", (col("year").cast("int") - Year.now.getValue) * -1)
 
+    df.show()
   }
+
 }
